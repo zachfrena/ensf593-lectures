34c5676b030c012dfdc2640d90bfba8139b25da0
Yves <12809436+ypauchard@users.noreply.github.com>
2021-05-10 11:08:08 -0600

    l02 OneRowNim work.

--- l02/src/OneRowNim.java
+++ l02/src/OneRowNim.java
@@ -1,4 +1,7 @@
 public class OneRowNim {
+	
+	//private static int MAX_STICKS = 100;
+	
 	private int nSticks = 7; // Start with 7 sticks .
 	private int player = 1; // Player 1 plays first .
 
@@ -17,10 +20,15 @@ public class OneRowNim {
 		player = 3 - player;
 	} // takeThree ()
 
+	/**
+	 * Prints number of sticks and next player.
+	 */
 	public void report() {
-		//TODO print Number of sticks left: 
 		
+		//TODO print Number of sticks left: 
+		System.out.println("Number of sticks left: "+nSticks);
 		//TODO print who has next turn
+		System.out.println("Next turn Player "+player);
 		
 	} // report ()
 } // OneRowNim1 class
--- l02/src/OneRowNimTester.java
+++ l02/src/OneRowNimTester.java
@@ -4,23 +4,34 @@ public class OneRowNimTester {
 	public static void main(String[] args) {
 		
 		//TODO create a OneRowNim object
-		
+		OneRowNim nim = new OneRowNim();
 		
 		// 7 sticks, player 1
 		// TODO call report
+		nim.report();
 		
 		// TODO take three sticks
+		nim.takeThree();
 		// 4 sticks, player 2
 		// TODO call report
+		nim.report();
 		
-		// TODO take three sticks
+		// TODO take two sticks
+		nim.takeTwo();
 		// 2 sticks, player 1
 		// TODO call report
+		nim.report();
 		
 		
 		// TODO take one stick
+		nim.takeOne();
 		// 1 stick, player 2
 		// TODO call report
+		nim.report();
+		
+		// TODO This will go negative
+		nim.takeThree();
+		nim.report();
 		
 				
 	}
