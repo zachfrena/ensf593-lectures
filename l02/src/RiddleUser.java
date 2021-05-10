a602e3320f77f102187fb2ed64f0cb151ecb7882
Yves <12809436+ypauchard@users.noreply.github.com>
2021-05-10 11:07:42 -0600

    l02 Riddle work.

--- l02/src/RiddleUser.java
+++ l02/src/RiddleUser.java
@@ -7,25 +7,36 @@
  */
 public class RiddleUser {
 	
+	private Riddle riddle;
+	
 
+	/**
+	 * Sets up 2 riddles and prints their question and answer
+	 * 
+	 * @param argv
+	 */
 	public static void main(String argv[]) { 
 		Riddle riddle1 = new Riddle (
 			"What is black and white and red all over?",
 			"An embarrassed zebra."); 
 		
 		//TODO add a second riddle
+		Riddle riddle2 = new Riddle("Why?", "Because.");
 		
 		System.out.println("Here are two riddles:");
 		System.out.println( riddle1 . getQuestion ( ) ) ;
 		
 		//TODO print question of second riddle
+		System.out.println(riddle2.getQuestion());
 		 
 		
 		System.out.println("The answer to the first riddle is:"); 
 		//TODO print answer to first riddle
+		System.out.println(riddle1.getAnswer());
 		
 		System.out.println("The answer to the second is:"); 
 		// TODO print answer to second riddle
+		System.out.println(riddle2.getAnswer());
 		
 		}
 
